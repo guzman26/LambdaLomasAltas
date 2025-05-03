f2cb384a32b514ac8835c11904f0821b47bcfe6a
FELIPE GUZMAN <fguzma@uc.cl>
2025-05-02 21:42:26 -0400

    mix

--- handlers/getPackingEggs.js
+++ handlers/getPackingEggs.js
@@ -1,10 +1,10 @@
 const getBoxes = require('./getBoxes');
-const db = require('../utils/db');
 const response = require('../utils/response');
+const { getBoxesByLocation } = require('../models/boxes');
 
 module.exports = async () => {
   try {
-    const eggs = await db.getBoxesByLocation('PACKING');
+    const eggs = await getBoxesByLocation('PACKING');
     return response(200, eggs);
   } catch (error) {
     return response(500, 'Error al obtener huevos en PACKING', { error: error.message });
